1df81d21814781ea3290c3f19d17f2efda35590b
Mirjam Friesen <mirjam.friesen@stritzinger.com>
2026-03-20 16:38:15 +0100

    Add SPDX header

--- apps/rebar/src/rebar_prv_escriptize.erl
+++ apps/rebar/src/rebar_prv_escriptize.erl
@@ -1,10 +1,13 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
 %% ex: ts=4 sw=4 et
-%% -------------------------------------------------------------------
+
+%% %CopyrightBegin%
 %%
-%% rebar: Erlang Build Tools
+%% SPDX-License-Identifier: MIT
 %%
 %% Copyright (c) 2009 Dave Smith (dizzyd@dizzyd.com)
+%% Copyright (c) 2015-2026 Rebar3 and its contributors
+%% Copyright (c) 2026 Dipl. Phys. Peer Stritzinger GmbH
 %%
 %% Permission is hereby granted, free of charge, to any person obtaining a copy
 %% of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +26,9 @@
 %% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 %% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 %% THE SOFTWARE.
-%% -------------------------------------------------------------------
+%%
+%% %CopyrightEnd%
+
 -module(rebar_prv_escriptize).
 
 -behaviour(provider).
@@ -342,4 +347,3 @@ write_windows_script(Target, _) ->
         "@echo off\r\n"
         "escript.exe \"%~dpn0\" %*\r\n",
     ok = file:write_file(CmdPath, CmdScript).
-
--- apps/rebar/src/rebar_prv_shell.erl
+++ apps/rebar/src/rebar_prv_shell.erl
@@ -1,10 +1,13 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
 %% ex: ts=4 sw=4 et
-%% -------------------------------------------------------------------
 %%
-%% rebar: Erlang Build Tools
+%% %CopyrightBegin%
+%%
+%% SPDX-License-Identifier: MIT
 %%
 %% Copyright (c) 2011 Trifork
+%% Copyright (c) 2015-2026 Rebar3 and its contributors
+%% Copyright (c) 2026 Dipl. Phys. Peer Stritzinger GmbH
 %%
 %% Permission is hereby granted, free of charge, to any person obtaining a copy
 %% of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +26,8 @@
 %% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 %% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 %% THE SOFTWARE.
-%% -------------------------------------------------------------------
+%%
+%% %CopyrightEnd%
 
 -module(rebar_prv_shell).
 -author("Kresten Krab Thorup <krab@trifork.com>").
