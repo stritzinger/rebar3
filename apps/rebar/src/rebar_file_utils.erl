cebfb92c567e6ed1ffb23507edb61deba0bed633
GwendalLaurent <gwendal.laurent@stritzinger.com>
2026-03-31 16:59:25 +0200

    use MIT license instead of Apache

--- apps/rebar/src/rebar_file_utils.erl
+++ apps/rebar/src/rebar_file_utils.erl
@@ -1,32 +1,12 @@
 %% %CopyrightBegin%
 %%
-%% SPDX-License-Identifier: Apache-2.0
+%% SPDX-Licence-Identifier: MIT
+%%
+%% SPDX-FileCopyrightText: Copyright 2009 Dave Smith (dizzyd@dizzyd.com)
 %%
 %% SPDX-FileCopyrightText: Copyright 2015-2026 Rebar3 and its contributors
 %%
 %% SPDX-FileCopyrightText: Copyright 2026 Dipl. Phys. Peer Stritzinger GmbH
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%%
-%%     http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS,
-%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%% See the License for the specific language governing permissions and
-%% limitations under the License.
-%%
-%% %CopyrightEnd%
-
-%% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
-%% ex: ts=4 sw=4 et
-%% -------------------------------------------------------------------
-%%
-%% rebar: Erlang Build Tools
-%%
-%% Copyright (c) 2009 Dave Smith (dizzyd@dizzyd.com)
 %%
 %% Permission is hereby granted, free of charge, to any person obtaining a copy
 %% of this software and associated documentation files (the "Software"), to deal
@@ -45,7 +25,7 @@
 %% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 %% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 %% THE SOFTWARE.
-%% -------------------------------------------------------------------
+%% %CopyrightEnd%
 
 -module(rebar_file_utils).
 
