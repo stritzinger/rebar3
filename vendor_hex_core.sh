57f7856ff64c6ef6eec59983f0c8618c5dfba69f
Jonatan Männchen <jonatan@maennchen.ch>
2026-04-09 21:52:33 +0200

    Inline vendor.sh from hex_core
    hex_core is deleting the script

--- vendor_hex_core.sh
+++ vendor_hex_core.sh
@@ -33,8 +33,71 @@ REBAR_TOP=$(pwd)/apps/rebar
 export REBAR_TOP
 pushd "$1"
 touch proto/* # force re-generation of protobuf elements
-TARGET_ERLANG_VERSION=26
+TARGET_ERLANG_VERSION=29
 export TARGET_ERLANG_VERSION
 rebar as dev compile
-./vendor.sh "$REBAR_TOP/src/vendored" rb_
+
+source_dir=src
+target_dir=src
+prefix=rb_
+hex_core_version=`cat $source_dir/hex_core.hrl | grep HEX_CORE_VERSION | cut -d'"' -f2`
+
+filenames="hex_core.hrl \
+           hex_core.erl \
+           hex_erl_tar.erl \
+           hex_erl_tar.hrl \
+           hex_pb_names.erl \
+           hex_pb_package.erl \
+           hex_pb_signed.erl \
+           hex_pb_versions.erl \
+           hex_tarball.erl \
+           hex_registry.erl \
+           hex_http_httpc.erl \
+           hex_http.erl \
+           hex_repo.erl \
+           hex_api.erl \
+           hex_api_key.erl \
+           hex_api_oauth.erl \
+           hex_api_package.erl \
+           hex_api_package_owner.erl \
+           hex_api_release.erl \
+           hex_api_user.erl \
+           hex_licenses.erl \
+           hex_safe_binary_to_term.erl \
+           safe_erl_term.xrl"
+
+search_to_replace="hex_core: \
+                   hex_core) \
+                   hex_core.hrl \
+                   hex_erl_tar \
+                   hex_pb_names \
+                   hex_pb_package \
+                   hex_pb_signed \
+                   hex_pb_versions \
+                   hex_registry \
+                   hex_tarball \
+                   hex_http \
+                   hex_repo \
+                   hex_api \
+                   hex_licenses \
+                   hex_safe_binary_to_term \
+                   safe_erl_term"
+
+rm -f $target_dir/$prefix*
+
+for filename in $filenames; do
+  source_path=$source_dir/$filename
+  target_path=$target_dir/$prefix$filename
+
+  echo "%% Vendored from hex_core v$hex_core_version, do not edit manually" > $target_path
+  echo >> $target_path
+  cat $source_path >> $target_path
+
+  for word in $search_to_replace; do
+    sed -i.bak s/$word/$prefix$word/g $target_path
+    rm $target_path.bak
+  done
+done
+
+find src -regex '.*rb_.*' -exec mv -f {} "$REBAR_TOP/src/vendored" \;
 popd
